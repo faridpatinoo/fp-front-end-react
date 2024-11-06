af5645be0871aa7acf04d458c95dd3895e752cba
faridpatinoo <fc.patino5@gmail.com>
2024-11-06 14:49:36 -0500

    adding swiper library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './index.css';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
 function App() {
+  const [swiperRef, setSwiperRef] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Swiper
+        onSwiper={setSwiperRef}
+        slidesPerView={5}
+        spaceBetween={20}
+        pagination={{
+          type: 'fraction',
+        }}
+        navigation={true}
+        breakpoints={{
+          2228: {
+            slidesPerView: 6
+          },
+          1900: {
+            slidesPerView: 5,
+          },
+          1570: {
+            slidesPerView: 4,
+          },
+          1080: {
+            slidesPerView: 3,
+          },
+          684: {
+            slidesPerView: 2,
+          },
+          500: {
+            slidesPerView: 1,
+          },
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+      </Swiper>
+    </>
   );
 }
 
