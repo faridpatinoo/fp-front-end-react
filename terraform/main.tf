475290139d1925982f09f5c8075b4defb0f5fcc2
faridpatinoo <fc.patino5@gmail.com>
2025-01-26 20:13:31 -0500

    Update Terraform configuration for Azure resources and modify GitHub Actions workflow for deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,32 +4,22 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=4.16.0"
     }
   }
+
+  #tf state file will be stored in Azure Storage Account
   backend "azurerm" {
-    resource_group_name  = "fpplus-backend_group"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "fpplusstorageaccount"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "terraform"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    resource_group_name  = "fpplus-backend_group" # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "fpplusstorageaccount" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "terraform"            # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "terraform.tfstate"    # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
-}
-
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network-2"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
+  features {
+  }
+  subscription_id = "fbe7b0f0-7cb6-44b3-8353-9ad095122ce3"
 }
